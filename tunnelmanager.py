b038747f24f595031657df901d7560dca0194eee
jsmoya <jsmoya@one-esecurity.com>
2018-07-08 17:30:49 +0200

    Limpieza de codigo Añadidas mas comprobaciones cuando no esta aceptado el fingerprint

--- tunnelmanager.py
+++ tunnelmanager.py
@@ -70,108 +70,169 @@ class Connection:
     def __repr__(self):
         return self.__str__()
 
+
 def read_config(config_file: str) -> list:
     # El formato de fichero esperado con lineas con el siguiente estilo: local,puerto_origen,ip,puerto_destino
 
-    with open(config_file, 'r') as f:
-        data = yaml.load(f)
+    try:
+        with open(config_file, 'r') as f:
+            data = yaml.load(f)
 
-        connections = list()
-        for c in data['connections']:
-            conn = Connection(c)
-            connections.append(conn)
-        return connections
+            conns = list()
+            for c in data['connections']:
+                conn = Connection(c)
+                conns.append(conn)
+            return conns
+    except IOError:
+        raise SyntaxError('Config file not found: {}'.format(config_file))
 
-    raise FileNotFoundError('Config file not found: {}'.format(config_file))
 
-def connect_ssh(connections: list) -> None:
+def connect_ssh(conns: list) -> None:
     # Try to connect
 
-    for c in connections:
+    for c in conns:
         cmd = c.get_cmd()
         global ssh_connections
+
+        print('    - Connecting {} ...'.format(c.name))
         ssh = pexpect.spawn(cmd)
         ssh_connections.append(ssh)
 
         try:
             time.sleep(0.1)
-            if debug_mode: print('SSH command: {}'.format(cmd))
+            if debug_mode:
+                print('SSH command: {}'.format(cmd))
+
             ret = ssh.expect(['yes/no', 'assword: ', '[#\$] '], timeout=5)
-            if debug_mode: print('RET: {} Output: {}'.format(ret, ssh.before))
+
+            if debug_mode:
+                print('RET: {} Output: {}'.format(ret, ssh.before))
 
             if ret == 0:
+
                 time.sleep(0.1)
                 ssh.sendline('yes')
                 time.sleep(0.1)
+
+                ret2 = ssh.expect(['assword: ', '[#\$] '], timeout=5)
+                if ret2 == 1:
+                    if not sys.stdin.isatty():
+                        if debug_mode:
+                            print('Password echo')
+
+                        ssh.sendline(sys.stdin.readline().rstrip())
+                    else:
+                        ssh.sendline(getpass.getpass('      Insert password for {}: '.format(c.name)))
+
+                    ret3 = ssh.expect(['Permission denied', '[#\$] '], timeout=5)
+                    if ret3 == 0:
+                        print('      *** Permission denied ***')
+                        continue
+
+                elif ret == 2:
+                    pass
+
             elif ret == 1:
                 if not sys.stdin.isatty():
-                    if debug_mode: print('Password echo')
+                    if debug_mode:
+                        print('Password echo')
+
                     ssh.sendline(sys.stdin.readline().rstrip())
                 else:
-                    ssh.sendline(getpass.getpass('Insert password for {}: '.format(c.name)))
+                    ssh.sendline(getpass.getpass('      Insert password for {}: '.format(c.name)))
+
+                ret2 = ssh.expect(['Permission denied', '[#\$] '], timeout=5)
+                if ret2 == 0:
+                    print('      *** Permission denied ***')
+                    continue
 
             elif ret == 2:
                 pass
 
         except pexpect.TIMEOUT:
             # Excepcion por tiempo de conexion
-            print('Timeout error {}'.format(c.name))
+            print('      *** Timeout error {} ***'.format(c.name))
+            continue
         except pexpect.exceptions.EOF:
-            print('Connection error {}'.format(c.name))
+            print('      *** Connection error {} ***'.format(c.name))
             continue
 
-        if debug_mode:
-            print('    - {} connected\n'.format(c.name))
-        else:
-            print('    - {} connected'.format(c.name))
+        print('      {} connected\n'.format(c.name))
+
 
 def sleep_loop():
-    '''Bucle infinito hasta pulsar control c'''
+    """Bucle infinito hasta pulsar control c"""
     try:
         while True:
             time.sleep(2)
     except KeyboardInterrupt:
         sys.exit(0)
 
+
 def get_connections_names():
+    global connections
     return [c.name for c in connections]
 
-def sort_connections():
-    global connections
 
-    connections_sorted = list()
-    for c1 in connections:
-        for c2 in connections:
+def sort_connections(conns):
+    d = conns
+    new_connections = list()
+    while len(d) > 0:
+        i, d = sort_connections_alt(d)
+        new_connections = new_connections + i
+
+    return new_connections
+
+
+def sort_connections_alt(conns):
+
+    # Lista de conexiones independientes y dependientes
+    i = list()
+    d = list()
+    for c1 in conns:
+        depend = False
+        for c2 in conns:
             if c1.port in c2.source_local_ports:
                 depend = True
+                d.append(c1)
+                break
+
+        if not depend:
+            i.append(c1)
+
+    return i, d
 
 
 @begin.subcommand
 def connectall():
     """Connect all ssh configured ssh connections"""
-    print('Connecting all ssh connections')
+    print('Connecting all ssh connections\n')
+    global connections
     connect_ssh(connections)
-    print('All connected')
+    print('\nProcess finished')
     sleep_loop()
+
 
 @begin.subcommand
 def connect(names):
     """Connect given ssh connections"""
     names_list = names.split(',')
-    print('Connecting {}'.format(names))
+    print('Connecting {}\n'.format(names))
 
     # Encontrar algun error en conexion pasada para informar al usuario
     connections_names = get_connections_names()
     ignored_connections = [c for c in names_list if c not in connections_names]
 
     if len(ignored_connections) > 0:
-        print('Connections not found and ignoring: {}'.format(', '.join(ignored_connections)))
+        print('WARNING: Connections not found and ignoring: {}'.format(', '.join(ignored_connections)))
 
     # Filtrar conexiones y conectar
+    global connections
     connections_filtered = [c for c in connections if c.name in names_list]
     connect_ssh(connections_filtered)
-    print('All connected')
+    print('\nProcess finished')
     sleep_loop()
+
 
 @begin.subcommand
 def listconnections():
@@ -182,6 +243,7 @@ def listconnections():
     connections_names = get_connections_names()
     for name in connections_names:
         print('    {}'.format(name))
+
 
 @begin.start(auto_convert=True)
 def main(config_file: 'Config file' = 'tunnels.yaml', debug: 'Debug mode' = False):
@@ -194,11 +256,11 @@ def main(config_file: 'Config file' = 'tunnels.yaml', debug: 'Debug mode' = Fals
         connections = read_config(config_file)
 
         # Ordenar conexiones jerarquicamente para que no haya ningun problema de dependencias
-        # sort_connections()
+        connections = sort_connections(connections)
 
         # Modo debug
         debug_mode = debug
 
-    except FileNotFoundError:
+    except SyntaxError:
         print('ERROR: File {} not found'.format(config_file))
         sys.exit(1)
